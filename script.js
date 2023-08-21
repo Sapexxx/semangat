5f56f6b13dd7ff8e7fdc50f422c52c7025fd12a7
Sapexae_ <86524630+Sapexxx@users.noreply.github.com>
2023-08-22 00:09:24 +0700

    first Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to show images when button is clicked
+function showImages(imagesId) {
+    const productImages = document.getElementById(imagesId);
+    productImages.classList.toggle("active");
+}
